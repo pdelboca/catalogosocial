f4fb52c067b3f5d2642b877255ccc0e0b0a5f3cd
pdelboca <patriciodelboca@gmail.com>
2024-01-05 17:42:44 -0300

    Original URLs are no longer needed in the model

--- catalogo/models.py
+++ catalogo/models.py
@@ -6,14 +6,9 @@ class DatasetExtras(models.Model):
     dataset = models.OneToOneField(
         Dataset, on_delete=models.CASCADE, related_name="extras"
     )
-    original_landing_page = models.URLField(blank=True, null=True)
-    original_id = models.CharField(max_length=255, blank=True)
 
 
 class DistributionExtras(models.Model):
     distribution = models.OneToOneField(
         Distribution, on_delete=models.CASCADE, related_name="extras"
     )
-    original_access_url = models.URLField(blank=True, null=True)
-    original_download_url = models.URLField(blank=True, null=True)
-    original_id = models.CharField(max_length=255, blank=True)
